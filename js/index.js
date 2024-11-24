ab14628d8d2ea64c77b51e4d090c3feb93b97adb
zaenishi <zaenishi@gmail.com>
2024-11-24 16:10:15 +0700

    first

--- js/index.js
+++
@@ -1,8 +0,0 @@
-fetch('../db/index.json')
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById('message').textContent = data.system;
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
